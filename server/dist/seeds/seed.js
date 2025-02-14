ec2624411b22fc9114baa507bd8bc890a34f245d
FORTIS Roberto <roberto.4ti@gmail.com>
2025-02-13 20:41:37 -0500

    update

--- server/dist/seeds/seed.js
+++ server/dist/seeds/seed.js
@@ -1,10 +1,14 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
-import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+// import pythonQuestions from './pythonQuestions.json' assert { type: "json" };
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const questionData = require('./pythonQuestions.json');
 db.once('open', async () => {
     await cleanDB('Question', 'questions');
-    await Question.insertMany(pythonQuestions);
+    // await Question.insertMany(pythonQuestions);
+    await Question.insertMany(questionData);
     console.log('Questions seeded!');
     process.exit(0);
 });
