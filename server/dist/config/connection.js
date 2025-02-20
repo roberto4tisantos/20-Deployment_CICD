503dfac2cc74381b764275c2ba0beef11ba6dc09
FORTIS Roberto <roberto.4ti@gmail.com>
2025-02-19 21:55:57 -0500

    update

--- server/dist/config/connection.js
+++ server/dist/config/connection.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+mongoose.connect(process.env.MONGODB_URI || '');
 export default mongoose.connection;
--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+mongoose.connect(process.env.MONGODB_URI || '');
 
 export default mongoose.connection;
