5d526b3c9b9c0eb25f8f4ca95da8704a9a3108cb
FORTIS Roberto <roberto.4ti@gmail.com>
2025-02-20 18:45:34 -0500

    update

--- server/dist/config/connection.js
+++ server/dist/config/connection.js
@@ -3,4 +3,5 @@ dotenv.config();
 import mongoose from 'mongoose';
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
 mongoose.connect(process.env.MONGODB_URI || '');
+// mongoose.connect(process.env.MONGODB_ATLAS || '');
 export default mongoose.connection;
--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -5,5 +5,6 @@ import mongoose from 'mongoose';
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
 mongoose.connect(process.env.MONGODB_URI || '');
+// mongoose.connect(process.env.MONGODB_ATLAS || '');
 
 export default mongoose.connection;
